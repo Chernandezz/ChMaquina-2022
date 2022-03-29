8acd0eafbf6f49abec69782c33d181cf5d8074fd
Crisitian Felipe Hernandez Zuluaga <chernandezz@unal.edu.co>
2022-03-29 14:34:47 -0500

    Se creo el array de memoria principal y se comento el codigo

--- script.js
+++ script.js
@@ -14,12 +14,15 @@ arreglo = new Array(7100);
 
 // Variables Usadas
 let input = document.getElementById("upload");
+let ejecutar = document.getElementById("ejecutar");
 let textarea = document.getElementById("memoria");
 let linea = document.getElementById("linea");
 let codigo = document.getElementById("codigo");
 let iconoLinea = document.getElementById("iconoLinea");
 let cont = 0;
 let memoriaDefinida = false;
+let lines = [];
+let memoriaprincipal = [];
 
 // Valores Kernel y Memoria
 let tamkernel = document.getElementById("tamkernel").value;
@@ -27,11 +30,13 @@ let tammemoria = document.getElementById("tammemoria").value;
 
 // EvenListener del boton de carga de archivos
 input.addEventListener("change", () => {
+  // Se desabilita el poder cambiar memoria y kernel
   document.getElementById("config").disabled = true;
   document.getElementById("config").classList.add("desactivado");
 
   let files = input.files;
 
+  // Validacion para Ver si el usuario cargo algo
   if (files.length == 0) return;
 
   const file = files[0];
@@ -39,27 +44,31 @@ input.addEventListener("change", () => {
   let reader = new FileReader();
 
   reader.onload = (e) => {
+    // Entrada del archivo cargado
     const file = e.target.result;
-    const lines = file.split(/\r\n|\n/);
+    lines = file.split(/\r\n|\n/);
+    // Eliminacion de lineas vacias
     for (let index = 0; index < lines.length + 2; index++) {
       if (lines[index] == "") {
         lines.splice(index, 1);
       }
     }
-    console.log(lines);
     cont += lines.length;
     if (cont > tammemoria - tamkernel) {
       alert("ERROR, Memoria Insuficiente");
       return;
     }
+    memoriaprincipal.push(lines);
     if (!memoriaDefinida) {
+      // Salida de numero de lineas
       for (let index = 1; index <= tammemoria; index++) {
         let tag = document.createElement("div");
         let text = document.createTextNode(index);
         tag.appendChild(text);
         linea.appendChild(tag);
-        memoriaDefinida = true;
       }
+
+      // Icono de Kernel
       for (let index = 0; index < tamkernel; index++) {
         let tag = document.createElement("div");
         let tagi = document.createElement("i");
@@ -68,6 +77,8 @@ input.addEventListener("change", () => {
         tag.appendChild(tagi);
         iconoLinea.appendChild(tag);
       }
+
+      // Icono de lineas de codigo
       for (let index = 0; index < tammemoria - tamkernel; index++) {
         let tag = document.createElement("div");
         let tagi = document.createElement("i");
@@ -76,8 +87,11 @@ input.addEventListener("change", () => {
         tag.appendChild(tagi);
         iconoLinea.appendChild(tag);
       }
+      // Confitmacion de que ya se mostraron los numeros de linea con los iconos
+      memoriaDefinida = true;
     }
 
+    // Salida de la linea --kernel--
     for (let index = 0; index < tamkernel; index++) {
       let tag = document.createElement("div");
       let text = document.createTextNode("--- KERNEL ---");
@@ -85,6 +99,7 @@ input.addEventListener("change", () => {
       codigo.appendChild(tag);
     }
 
+    // Salida de cada linea de codigo en el array lines
     for (let index = 0; index < lines.length; index++) {
       if (lines[index] == "") {
         continue;
@@ -103,10 +118,18 @@ input.addEventListener("change", () => {
   reader.readAsText(file);
 });
 
+ejecutar.addEventListener("click", () => {
+  lines.forEach((element) => {
+    console.log(element);
+  });
+});
+
+// Evento que recive cuando se cambia el tam de Kernel
 function actualizarValorKernel() {
   tamkernel = document.getElementById("tamkernel").value;
 }
 
+// Evento que recive cuando se cambia el tam de memoria
 function actualizarValorMemoria() {
   tammemoria = document.getElementById("tammemoria").value;
 }
